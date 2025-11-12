15b2abe811b9c86cd0ace67d4c231aa5519a62d8
Muhammad Irsadul Ibad_5124521007 <irsadul220406@gmail.com>
2025-11-12 13:06:58 +0700

    Gameplay Mechanics

---
+++ Prototype 4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    private float spawnRangeX = 9;
+    private float spawnZMin = 15;
+    private float spawnZMax = 25;
+    public int enemyCount;
+    public int waveCount = 1;
+    public float enemySpeedIncrease = 0.5f;
+
+    void Start()
+    {
+        SpawnEnemyWave(waveCount);
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<EnemyX>().Length;
+
+        if (enemyCount == 0)
+        {
+            waveCount++;
+            SpawnEnemyWave(waveCount);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+            enemy.GetComponent<EnemyX>().speed += enemySpeedIncrease * waveCount;
+        }
+
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+}
